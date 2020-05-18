4896eee55a0db6390f02a5133060a1902fa6aaa8
andreiCobilas <andrei7xd@yahoo.com>
2020-05-18 14:00:37 +0300

    Made minor changes, template for future test added

--- src/test/java/com/endava/intership/Framework/Infrastructure/Driver/Wait.java
+++ src/test/java/com/endava/intership/Framework/Infrastructure/Driver/Wait.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class Wait {
+
     private WebDriver driver;
 
     public Wait(WebDriver driver) {
@@ -17,26 +18,26 @@ public class Wait {
     }
 
     private void waitUntilCondition(ExpectedCondition condition, String timeoutMessage, int timeout) {
-        WebDriverWait wait = new WebDriverWait(driver, timeout);
-        wait.withMessage(timeoutMessage);
-        wait.until(condition);
+        WebDriverWait wait = new WebDriverWait( driver, timeout );
+        wait.withMessage( timeoutMessage );
+        wait.until( condition );
     }
 
-    public void forLoading(int timeout){
-        ExpectedCondition<Object> condition = ExpectedConditions.jsReturnsValue("return document.readyState==\"complete\";");
-        String timeoutMessage = "Page didn't load after " + Integer.toString(timeout) + " seconds.";
-        waitUntilCondition(condition, timeoutMessage, timeout);
+    public void forLoading(int timeout) {
+        ExpectedCondition<Object> condition = ExpectedConditions.jsReturnsValue( "return document.readyState==\"complete\";" );
+        String timeoutMessage = "Page didn't load after " + Integer.toString( timeout ) + " seconds.";
+        waitUntilCondition( condition, timeoutMessage, timeout );
     }
 
-    public void forElementToBeDisplayed(int timeout, WebElement webElement, String webElementName){
-        ExpectedCondition<WebElement> condition = ExpectedConditions.visibilityOf(webElement);
-        String timeoutMessage = webElementName + " wasn't displayed after " + Integer.toString(timeout) + " seconds.";
-        waitUntilCondition(condition, timeoutMessage, timeout);
+    public void forElementToBeDisplayed(int timeout, WebElement webElement, String webElementName) {
+        ExpectedCondition<WebElement> condition = ExpectedConditions.visibilityOf( webElement );
+        String timeoutMessage = webElementName + " wasn't displayed after " + Integer.toString( timeout ) + " seconds.";
+        waitUntilCondition( condition, timeoutMessage, timeout );
     }
 
-    public void forPresenceOfElements(int timeout, By elementLocator, String elementName){
-        ExpectedCondition<List<WebElement>> condition = ExpectedConditions.presenceOfAllElementsLocatedBy(elementLocator);
-        String timeoutMessage = elementName + " elements were not displayed after " + Integer.toString(timeout) + " seconds.";
-        waitUntilCondition(condition, timeoutMessage, timeout);
+    public void forPresenceOfElements(int timeout, By elementLocator, String elementName) {
+        ExpectedCondition<List<WebElement>> condition = ExpectedConditions.presenceOfAllElementsLocatedBy( elementLocator );
+        String timeoutMessage = elementName + " elements were not displayed after " + Integer.toString( timeout ) + " seconds.";
+        waitUntilCondition( condition, timeoutMessage, timeout );
     }
 }
--- src/test/resources/feature/Clicking_X_Cart.feature
+++ src/test/resources/feature/Clicking_X_Cart.feature
@@ -1,8 +1,14 @@
 Feature: Home button
 
   @Random_test
-  Scenario: Testing the page
+  Scenario Outline: Testing the page
     Given User is on homepage
+    Then Coming soon is clicked
+    And Home page is clicked
+    And Sign in is clicked
+    And "<mail>" is entered
+    And "<password>" are entered
 
-
-
+    Examples:
+      | mail | password |
+      | test@mail.com | teststuff |
