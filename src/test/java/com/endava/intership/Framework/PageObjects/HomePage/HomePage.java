8b2372c3d9d1c03f2485d8da9cd0c1dd6f81d6a1
andreiCobilas <andrei7xd@yahoo.com>
2020-05-18 14:52:48 +0300

    Made minor changes, template for future test added, cleanned code

--- src/test/java/com/endava/intership/Framework/PageObjects/HomePage/HomePage.java
+++ src/test/java/com/endava/intership/Framework/PageObjects/HomePage/HomePage.java
@@ -2,10 +2,7 @@ package com.endava.intership.Framework.PageObjects.HomePage;
 
 import com.endava.intership.Framework.Infrastructure.Driver.Setup;
 import com.endava.intership.Framework.PageObjects.DefaultPage.DefaultPage;
-import io.cucumber.java.en.Given;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends DefaultPage {
